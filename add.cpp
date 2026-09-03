b4e8a1ebe1c447885eb8182c39f9cb7bb85e93c8
vedikaappani <vedikaappani@gmail.com>
2026-09-03 17:23:25 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
-using namspace std;
+using namespace std;
 int main()
 {
 int a,b,sum;
 cout<<"enter value for a and b:";
 cin>>a>>b;
 sum=a+b;
-cout<<"sum of this is:";
+cout<<"sum of this is:"<<sum;
 return 0;
 }
