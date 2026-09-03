0bb51f8897113599eeab9b3e0b143ddd6e0c84cc
vedikaappani <vedikaappani@gmail.com>
2026-09-03 17:34:13 +0530

    Update swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -5,11 +5,11 @@ int main()
 int a,b,c=0;
 cout<<"enter values for a and b:";
 cin>>a>>b;
-cout<<"before swapping:"<<endl;
+cout<<"before swapping:"<<"a="<<a<<"b="<<b<<endl;
 c=a;
 a=b;
 b=c;
-cout<<"a="<<a<<endl;
+cout<<"after swapping"<<"a="<<a<<endl;
 cout<<"b="<<b<<endl;
 return 0;
 }
